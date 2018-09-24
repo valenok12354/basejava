c7b83f0acd9e721493e52ef17f762491cdf1e19c
Igor Koiv <koiv.igor@gmail.com>
2018-09-24 16:31:24 +0300

    new loop of the 1st task

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,47 @@
-/**
- * Array based storage for Resumes
- */
+
+import java.util.Arrays;
+
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    private Resume[] storage = new Resume[10000];
+    private int count = 0;
 
     void clear() {
+        for (Resume aStorage : storage) {
+            if (aStorage != null) {
+                Arrays.fill(storage, null);
+            }
+        }
     }
 
     void save(Resume r) {
+        storage[count] = r;
+        count++;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < count; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < count; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                System.arraycopy(storage, i + 1, storage, i, storage.length - i - 1);
+            }
+            break;
+        }
     }
 
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, count);
     }
 
     int size() {
-        return 0;
+        return count;
     }
 }
+
